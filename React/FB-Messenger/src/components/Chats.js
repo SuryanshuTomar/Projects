fc39ac89954a00f6b24c0eab15f24ee576184dfe
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-03-29 06:25:19 +0530

    Added Chat Functionality And ENV

--- React/FB-Messenger/src/components/Chats.js
+++ React/FB-Messenger/src/components/Chats.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 
 import { useHistory } from "react-router-dom";
 import { ChatEngine } from "react-chat-engine";
 import { auth } from "../firebase";
 import { useAuth } from "../contexts/AuthContext";
+import axios from "axios";
 
 const Chats = (props) => {
+	const [loading, setLoading] = useState(true);
 	const history = useHistory();
 	const { user } = useAuth();
 
@@ -17,6 +19,55 @@ const Chats = (props) => {
 		history.push("/");
 	};
 
+	const getFile = async (url) => {
+		const response = await fetch(url);
+		console.log(response, url);
+		const data = await response.blob();
+
+		return new File([data], "userPhoto.jpg", { type: "image/jpeg" });
+	};
+
+	useEffect(() => {
+		// If there is not user then Logout
+		if (!user) {
+			history.push("/");
+			return;
+		}
+
+		axios
+			.get("https://api.chatengine.io/users/me", {
+				headers: {
+					"project-id": process.env.REACT_APP_CHAT_ENGINE_ID,
+					"user-name": user.email,
+					"user-secret": user.uid,
+				},
+			})
+			.then(() => {
+				setLoading(false);
+			})
+			.catch(() => {
+				let formData = new FormData();
+				formData.append("email", user.email);
+				formData.append("username", user.email);
+				formData.append("secret", user.uid);
+
+				getFile(user.photoURL).then((avatar) => {
+					formData.append("avatar", avatar, avatar.name);
+
+					axios
+						.post("https://api.chatengine.io/users", formData, {
+							headers: {
+								"private-key": process.env.REACT_APP_CHAT_ENGINE_KEY,
+							},
+						})
+						.then(() => setLoading(false))
+						.catch((error) => console.log(error));
+				});
+			});
+	}, [user, history]);
+
+	if (!user || loading) return "Loading...";
+
 	return (
 		<div className="chats-page">
 			<div className="nav-bar">
@@ -26,10 +77,10 @@ const Chats = (props) => {
 				</div>
 			</div>
 			<ChatEngine
-				height="calc(100vh - 66px)"
-				projectId="39d1a9b0-8601-4c14-82d8-93ee82d60253"
-				userName="."
-				userSecret="."
+				height="calc(100vh - 70px)"
+				projectID={process.env.REACT_APP_CHAT_ENGINE_ID}
+				userName={user.email}
+				userSecret={user.uid}
 			></ChatEngine>
 		</div>
 	);
