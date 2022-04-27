91380cc05ce59f4da6d8b9c9c52b4623770f6f30
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-04-27 21:52:52 +0530

    fetch popular recipe

--- React/lecious/src/components/Popular.js
+++ React/lecious/src/components/Popular.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function Popular() {
-	
-	return <div>popular</div>;
+	const [popular, setPopular] = useState([]);
+
+	const getPopular = async () => {
+		const response = await fetch(
+			`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_SPOONACULAR_API_KEY}&number=9`
+		);
+		const data = await response.json();
+		console.log(data.recipes);
+		setPopular(data.recipes);
+	};
+
+	useEffect(() => {
+		getPopular();
+	}, []);
+
+	return (
+		<div>
+			{popular.map((recipe) => (
+				<div key={recipe.id}>
+					<p>{recipe.title}</p>
+				</div>
+			))}
+		</div>
+	);
 }
 
 export default Popular;
