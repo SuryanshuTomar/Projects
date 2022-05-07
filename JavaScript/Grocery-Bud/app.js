a34fc2b2c2e2ed9c02819ec2b554fea9b2881aff
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-05-07 22:29:12 +0530

    added display alert functionality on submit

--- JavaScript/Grocery-Bud/app.js
+++ JavaScript/Grocery-Bud/app.js
@@ -1,12 +1,46 @@
 // ****** SELECT ITEMS **********
+const alert = document.querySelector(".alert");
+const form = document.querySelector(".grocery-form");
+const grocery = document.getElementById("grocery");
+const submitBtn = document.querySelector(".submit-btn");
+const container = document.querySelector(".grocery-conatiner");
+const list = document.querySelector(".grocery-list");
+const clearBtn = document.querySelector(".clear-btn");
 
 // edit option
+let editElement;
+let editFlag = false;
+let editId = "";
 
 // ****** EVENT LISTENERS **********
+// submit form
+form.addEventListener("submit", addItem);
 
 // ****** FUNCTIONS **********
+function addItem(event) {
+	event.preventDefault();
+	const value = grocery.value;
+	const id = new Date().getTime().toString();
+
+	if (value && !editFlag) {
+		console.log("Item Adding to list");
+	} else if (value && editFlag) {
+		console.log("Editing");
+	} else {
+		displayAlert("Empty Value", "danger");
+	}
+}
+
+// display alert
+const displayAlert = (text, action) => {
+	alert.textContent = text;
+	alert.classList.add(`alert-${action}`);
+	setTimeout(() => {
+		alert.textContent = "";
+		alert.classList.remove(`alert-${action}`);
+	}, 1200);
+};
 
 // ****** LOCAL STORAGE **********
 
 // ****** SETUP ITEMS **********
- 
