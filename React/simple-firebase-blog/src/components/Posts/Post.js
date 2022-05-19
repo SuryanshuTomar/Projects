89cf048d381e3c87ff2c527204318bc581ed67d1
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-05-19 22:01:30 +0530

    icons hover text

--- React/simple-firebase-blog/src/components/Posts/Post.js
+++ React/simple-firebase-blog/src/components/Posts/Post.js
@@ -19,40 +19,40 @@ function Post() {
 				<h4 className={styles["post-author"]}>@Author</h4>
 				<div className={styles.react}>
 					<div>
-						<span>
+						<span title="Love">
 							<i className="fa-solid fa-heart"></i>
 							<p>1</p>
 						</span>
-						<span>
+						<span title="Like">
 							<i className="fa-solid fa-thumbs-up"></i>
 							<p>1</p>
 						</span>
-						<span>
+						<span title="Flushed">
 							<i className="fa-solid fa-face-flushed"></i>
 							<p>1</p>
 						</span>
-						<span>
+						<span title="Surprise">
 							<i className="fa-solid fa-face-surprise"></i>
 							<p>1</p>
 						</span>
-						<span>
+						<span title="Hehe">
 							<i className="fa-solid fa-face-grin-stars"></i>
 							<p>1</p>
 						</span>
-						<span>
+						<span title="ROFL">
 							<i className="fa-solid fa-face-grin-squint-tears"></i>
 							<p>1</p>
 						</span>
-						<span>
+						<span title="Cry">
 							<i className="fa-solid fa-face-sad-cry"></i>
 							<p>1</p>
 						</span>
-						<span>
+						<span title="Ghost">
 							<i className="fa-solid fa-ghost"></i>
 							<p>1</p>
 						</span>
 					</div>
-					<span>
+					<span title="Delete Post">
 						<i className="fa-solid fa-trash"></i>
 					</span>
 				</div>
