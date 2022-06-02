4a47d0484b336061002788b9f50df9c9e67bb161
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-06-02 20:40:34 +0530

    Added Routing to components

--- React/dave-blog/src/App.js
+++ React/dave-blog/src/App.js
@@ -1,8 +1,28 @@
+import { useState, useEffect } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import NewPost from "./components/NewPost";
+import PostPage from "./components/PostPage";
+import About from "./components/About";
+import Missing from "./components/Missing";
+
 function App() {
 	return (
 		<div className="App">
-			<h1>Hello </h1>
-			<h2>Start editing to see some magic happen!</h2>
+			<Header />
+			<Nav />
+			<Routes>
+				<Route path="" element={<Home />} />
+				<Route path="post" element={<NewPost />} />
+				<Route path="post/:id" element={<PostPage />} />
+				<Route path="about" element={<About />} />
+				<Route path="*" element={<Missing />} />
+			</Routes>
+			<Footer />
 		</div>
 	);
 }
