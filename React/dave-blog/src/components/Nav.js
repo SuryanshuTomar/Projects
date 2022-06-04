e508a30808864238a51577b747f9090bced09980
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-06-04 20:50:24 +0530

    Added Nav Items and NavLinks

--- React/dave-blog/src/components/Nav.js
+++ React/dave-blog/src/components/Nav.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
-function Nav() {
-	return <nav>Nav</nav>;
+function Nav({ search, setSearch }) {
+	return (
+		<nav className="Nav">
+			<form onSubmit={(e) => e.preventDefault()} className="searchForm">
+				<label htmlFor="search">Search Posts</label>
+				<input
+					type="text"
+					id="search"
+					placeholder="Search Posts"
+					value={search}
+					onChange={(e) => setSearch(e.target.value)}
+				/>
+				<ul>
+					<li>
+						<NavLink to="/">Home</NavLink>
+					</li>
+					<li>
+						<NavLink to="post">Post</NavLink>
+					</li>
+					<li>
+						<NavLink to="about">About</NavLink>
+					</li>
+				</ul>
+			</form>
+		</nav>
+	);
 }
 
 export default Nav;
