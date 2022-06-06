02bf09a630818080b24ea0b5475b2637a175aa59
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-06-06 19:09:08 +0530

    New post Component

--- React/dave-blog/src/components/NewPost.js
+++ React/dave-blog/src/components/NewPost.js
@@ -1,7 +1,32 @@
 import React from "react";
 
-function NewPost() {
-	return <main>NewPost</main>;
+function NewPost(props) {
+	const { postTitle, setPostTitle, postBody, setPostBody, handleSubmit } =
+		props;
+
+	return (
+		<main className="NewPost">
+			<h2>New Post</h2>
+			<form onSubmit={handleSubmit} className="newPostForm">
+				<label htmlFor="postTitle">Title : </label>
+				<input
+					type="text"
+					id="postTitle"
+					value={postTitle}
+					onChange={(e) => setPostTitle(e.target.value)}
+					required
+				/>
+				<label htmlFor="postBody">Post : </label>
+				<textarea
+					id="postBody"
+					value={postBody}
+					onChange={(e) => setPostBody(e.target.value)}
+					required
+				/>
+				<button>Submit</button>
+			</form>
+		</main>
+	);
 }
 
 export default NewPost;
