f214eca1318c3001ad615e8f5984836ee8d3cd2b
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-06-06 19:15:54 +0530

    About Component

--- React/dave-blog/src/components/About.js
+++ React/dave-blog/src/components/About.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 function About() {
-	return <main>About</main>;
+	return (
+		<main className="About">
+			<h2>About</h2>
+			<p style={{ marginTop: "1rem" }}>
+				This Blog is a practise project and is made for learning purposes
+			</p>
+		</main>
+	);
 }
 
 export default About;
