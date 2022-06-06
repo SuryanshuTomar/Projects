e26b8cbfb9163dbe36ce467570a781fef4d0763d
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-06-06 19:15:46 +0530

    Footer Component

--- React/dave-blog/src/components/Footer.js
+++ React/dave-blog/src/components/Footer.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Footer() {
-	return <footer>Footer</footer>;
+	const today = new Date();
+	return (
+		<footer className="Footer">
+			<p>Copyright &copy; {today.getFullYear()}</p>
+		</footer>
+	);
 }
 
 export default Footer;
