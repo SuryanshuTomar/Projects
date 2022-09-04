037a3726b96069577277c22c11ac606a48d017f6
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-09-04 23:15:25 +0530

    Added UserName in Navbar

--- React/simple-firebase-blog/src/components/Navbar/Navbar.js
+++ React/simple-firebase-blog/src/components/Navbar/Navbar.js
@@ -1,15 +1,25 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Button from "../Button/Button";
 import styles from "./Navbar.module.css";
 import { NavLink } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
+import { auth } from "../../firebase/firebase-config";
 
 function Navbar() {
 	const { isAuth, signUserOut } = useContext(AuthContext);
+	const currentUser = auth?.currentUser;
+	localStorage.setItem("username", JSON.stringify(currentUser));
 	return (
 		<>
 			<div className={styles["nav-container"]}>
 				<div className={styles["navbar"]}>
+					{currentUser ? (
+						<h3>
+							{JSON.parse(localStorage.getItem("username")).displayName}
+						</h3>
+					) : (
+						""
+					)}
 					<NavLink to="/home">
 						<h3 className={styles["nav-link"]}>Home</h3>
 					</NavLink>
