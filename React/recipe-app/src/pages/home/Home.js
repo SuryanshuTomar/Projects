45e94d8f7e69db57b5831e2234b8f2aa4431c73c
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-09-15 16:26:44 +0530

    fetching data using useFetch hook

--- React/recipe-app/src/pages/home/Home.js
+++ React/recipe-app/src/pages/home/Home.js
@@ -1,8 +1,23 @@
-import React from "react";
+// import { useState } from "react";
 import styles from "./Home.module.css";
+import { useFetch } from "../../hooks/useFetch";
 
 function Home() {
-	return <div>Home</div>;
+	const {
+		data: recipes,
+		isPending,
+		error,
+	} = useFetch(" http://localhost:3000/recipes");
+
+	return (
+		<div className={styles.home}>
+			{error && <p className="error">{error}</p>}
+			{isPending && <p className="loading">Loading Recipes....</p>}
+			{recipes?.map((recipe) => (
+				<h2 key={recipe.id}>{recipe.title}</h2>
+			))}
+		</div>
+	);
 }
 
 export default Home;
