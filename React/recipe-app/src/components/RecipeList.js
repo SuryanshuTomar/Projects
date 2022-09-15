0910ce676198ac639e430b52f7b96ed3a2f9bd25
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-09-15 16:48:35 +0530

    Recipe List Component

---
+++ React/recipe-app/src/components/RecipeList.js
@@ -0,0 +1,25 @@
+// react
+import React from "react";
+
+// react-router
+import { Link } from "react-router-dom";
+
+//style
+import styles from "./Recipe.module.css";
+
+function RecipeList({ recipes }) {
+	return (
+		<div className={styles["recipe-list"]}>
+			{recipes.map((recipe) => (
+				<div key={recipe.id} className={styles.card}>
+					<h3>{recipe.title}</h3>
+					<p>{recipe.cookingTime} to make</p>
+					<div>{recipe.method.substring(0, 100)}.....</div>
+					<Link to={`/recipes/${recipe.id}`}>Cook This</Link>
+				</div>
+			))}
+		</div>
+	);
+}
+
+export default RecipeList;
