5c8ba4876da22b764b016e440e0bab10ed2109b8
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-09-17 17:41:34 +0530

    Theme Context setup

--- React/recipe-app/src/index.js
+++ React/recipe-app/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeContextProvider } from "./context/ThemeContext";
 
 ReactDOM.render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
+		<ThemeContextProvider>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</ThemeContextProvider>
 	</React.StrictMode>,
 	document.getElementById("root")
 );
