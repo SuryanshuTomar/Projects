6f17d432217bb7651ac0788b500a8440ff72c7b5
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-09-17 16:59:50 +0530

    search pages  logic for displaying searches

--- React/recipe-app/src/pages/search/Search.js
+++ React/recipe-app/src/pages/search/Search.js
@@ -1,8 +1,26 @@
+// react
 import React from "react";
+import { useSearchParams } from "react-router-dom";
+import RecipeList from "../../components/RecipeList";
+import { useFetch } from "../../hooks/useFetch";
+
+// styles
 import styles from "./Search.module.css";
 
 function Search() {
-	return <div>Search</div>;
+	const [queryString, setQueryString] = useSearchParams();
+	const query = queryString.get("query");
+	const url = `http://localhost:3000/recipes?q=${query}`;
+	const { data: searchRecipes, isPending, error } = useFetch(url);
+
+	return (
+		<div>
+			<h2 className="page-title">Recipes including "{query}"</h2>
+			{error && <p className="error">{error}</p>}
+			{isPending && <p className="loading">Loading Recipes....</p>}
+			{searchRecipes && <RecipeList recipes={searchRecipes} />}
+		</div>
+	);
 }
 
 export default Search;
