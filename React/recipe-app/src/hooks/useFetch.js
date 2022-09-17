78d55267c2e168c779d5e9c8b4775511c1e3a7df
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-09-17 15:51:19 +0530

    modified useFetch hook for HTTP POST request

--- React/recipe-app/src/hooks/useFetch.js
+++ React/recipe-app/src/hooks/useFetch.js
@@ -1,20 +1,34 @@
 import { useState, useEffect } from "react";
 
 // Custom Hook for fetching data
-export function useFetch(url) {
+export function useFetch(url, method = "GET") {
 	const [data, setData] = useState(null);
 	const [isPending, setIsPending] = useState(false);
 	const [error, setError] = useState(null);
+	const [options, setOptions] = useState(null);
+
+	const postData = (postData) => {
+		setOptions({
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(postData),
+		});
+	};
 
 	useEffect(() => {
 		// This Abort Controller class is JS itself and has nothing to do with reactjs
 		const controller = new AbortController();
 
-		const fetchData = async (url) => {
+		const fetchData = async (url, fetchOptions) => {
 			setIsPending(true);
 			try {
 				// setting the signal using about controllor instance from the AbortController class
-				const response = await fetch(url, { signal: controller.signal });
+				const response = await fetch(url, {
+					...fetchOptions,
+					signal: controller.signal,
+				});
 				if (!response.ok) {
 					throw new Error(response.statusText);
 				}
@@ -33,13 +47,20 @@ export function useFetch(url) {
 				}
 			}
 		};
-		fetchData(url);
+
+		if (method === "GET") {
+			fetchData(url);
+		}
+		
+		if (method === "POST" && options) {
+			fetchData(url, options);
+		}
 
 		// clean up function
 		return () => {
 			// aborting the fetch call in case our component in which useFetch is called gets unmounted
 			controller.abort();
 		};
-	}, [url]);
-	return { data, isPending, error };
+	}, [url, method, options]);
+	return { data, isPending, error, postData };
 }
