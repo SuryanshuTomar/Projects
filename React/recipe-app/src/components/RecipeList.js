268c1816d00612ea3c63da98b2e2a1e17d7630d8
SuryanshuTomar <suryanshutomar123@outlook.com>
2022-09-18 23:34:40 +0530

    delete recipe from firebase

--- React/recipe-app/src/components/RecipeList.js
+++ React/recipe-app/src/components/RecipeList.js
@@ -3,17 +3,34 @@ import React from "react";
 import { useTheme } from "../hooks/useTheme";
 
 // react-router
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 //style
 import styles from "./RecipeList.module.css";
+import deleteIcon from "../assets/delete_FILL0_wght400_GRAD0_opsz48.svg";
+
+// firebase
+import { projectFirestore } from "../firebase/config";
+import { doc, deleteDoc } from "firebase/firestore";
 
 function RecipeList({ recipes }) {
 	const { mode } = useTheme();
+	const navigate = useNavigate();
 
 	if (recipes.length === 0) {
 		return <div className="error">No Recipes found....</div>;
 	}
+
+	const deleteRecipeHandler = (docId) => {
+		const docRef = doc(projectFirestore, "recipes", docId);
+		deleteDoc(docRef)
+			.then((doc) => {
+				console.log(doc.data());
+			})
+			.catch((err) => console.log(err));
+	};
+
+	console.log(styles.delete);
 
 	return (
 		<div className={styles["recipe-list"]}>
@@ -26,6 +43,12 @@ function RecipeList({ recipes }) {
 					<p>{recipe.cookingTime} to make</p>
 					<div>{recipe.method.substring(0, 100)}.....</div>
 					<Link to={`/recipes/${recipe.id}`}>Cook This</Link>
+					<img
+						className={styles.delete}
+						src={deleteIcon}
+						alt="delete-icon"
+						onClick={() => deleteRecipeHandler(recipe.id)}
+					/>
 				</div>
 			))}
 		</div>
