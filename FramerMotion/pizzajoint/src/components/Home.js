fd96fbde76c0e98678ddd79a9b90347978ee23df
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-01-01 22:37:38 +0530

    added opacity animation to home container

--- FramerMotion/pizzajoint/src/components/Home.js
+++ FramerMotion/pizzajoint/src/components/Home.js
@@ -4,7 +4,11 @@ import { motion } from "framer-motion";
 
 const Home = () => {
 	return (
-		<motion.div className="home container" animate={{ rotateZ: -5 }}>
+		<motion.div
+			className="home container"
+			initial={{ opacity: 0 }}
+			animate={{ opacity: 1 }}
+		>
 			<h2>Welcome to Pizza Joint</h2>
 			<Link to="/base">
 				<button>Create Your Pizza</button>
