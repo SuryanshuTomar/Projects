d3e2132c07266a860d3e24fc94d97df5f4fe9f1b
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-01-20 20:25:22 +0530

    added single select component

--- TypeScript/SelectComponent-Lib/src/App.tsx
+++ TypeScript/SelectComponent-Lib/src/App.tsx
@@ -1,7 +1,17 @@
+import Select from "./components/Select";
+
+const options = [
+	{ label: "First", value: 1 },
+	{ label: "Second", value: 2 },
+	{ label: "Third", value: 3 },
+	{ label: "Fourth", value: 4 },
+	{ label: "Fifth", value: 5 },
+];
+
 function App() {
 	return (
 		<div>
-			Hi Lo
+			<Select options={options} />
 		</div>
 	);
 }
