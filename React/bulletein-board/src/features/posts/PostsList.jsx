0d787e6f66d18e1ac5b0e4d3e9fec9ab617fd6b8
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-01-27 19:54:15 +0530

    refractored the code according to the async thunk

--- React/bulletein-board/src/features/posts/PostsList.jsx
+++ React/bulletein-board/src/features/posts/PostsList.jsx
@@ -1,35 +1,49 @@
-import { useSelector } from "react-redux";
-import { selectAllPosts } from "./postsSlice";
-import React, { useState } from "react";
-import PostAuthor from "./PostAuthor";
-import TimeAgo from "./TimeAgo";
-import ReactionButtons from "./ReactionButtons";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import PostsExcerpt from "./PostsExcerpt";
+import {
+	selectAllPosts,
+	getPostsStatus,
+	getPostsError,
+	fetchPosts,
+} from "./postsSlice";
 
 const PostsList = () => {
 	const posts = useSelector(selectAllPosts);
+	const postsStatus = useSelector(getPostsStatus);
+	const postsError = useSelector(getPostsError);
+	const dispatch = useDispatch();
 
 	const [postAscending, setPostAscending] = useState(true);
+
+	// fetchPosts dispatch to fetch users asynchronously
+	useEffect(() => {
+		if (postsStatus === "idle") {
+			dispatch(fetchPosts());
+		}
+	}, [postsStatus, dispatch]);
 
 	// Post in ordered manner
 	// slice will return a new shallowcopy of the posts
 	// sort will sort the post according to the date posts are created
 	// localeCompare will return -1 or +1 accorrding to the date comparison
-	const orderedPosts = !postAscending
-		? posts.slice().sort((a, b) => b.date.localeCompare(a.date))
-		: posts;
+	// const orderedPosts = !postAscending
+	// 	? posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+	// 	: posts;
 
-	const renderedPosts = orderedPosts.map((post) => (
-		<article key={post.id}>
-			<h3>{post.title}</h3>
-			<p>{post.content.substring(0, 100)}</p>
-			<p className="postCredit">
-				<PostAuthor userId={post.userId} />
-				<TimeAgo timeStamp={post.date} />
-			</p>
-
-			<ReactionButtons post={post} />
-		</article>
-	));
+	let content;
+	if (postsStatus === "loading") {
+		content = <p>"Loading Posts...</p>;
+	} else if (postsStatus === "succeeded") {
+		const orderedPosts = !postAscending
+			? posts.slice().sort((a, b) => a.title.localeCompare(b.title))
+			: posts.slice().sort((a, b) => b.title.localeCompare(a.title));
+		content = orderedPosts.map((post) => (
+			<PostsExcerpt key={post.id * Math.random()} post={post} />
+		));
+	} else if (postsStatus === "failed") {
+		content = <p>{postsError}</p>;
+	}
 
 	return (
 		<section>
@@ -49,7 +63,7 @@ const PostsList = () => {
 					arrow_downward
 				</button>
 			</div>{" "}
-			{renderedPosts}
+			{content}
 		</section>
 	);
 };
