c7ca71a413eafa4dc5e828e78e3e2db999e5e7b0
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-02-01 22:15:58 +0530

    Header Component

---
+++ Redux-Toolkit/bulletein-board/src/components/Header.jsx
@@ -0,0 +1,21 @@
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+	return (
+		<header className="Header">
+			<h1>Redux Blog</h1>
+			<nav>
+				<ul>
+					<li>
+						<NavLink to="/">Home</NavLink>
+					</li>
+					<li>
+						<NavLink to="post">Add a Post</NavLink>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
+};
+
+export default Header;
