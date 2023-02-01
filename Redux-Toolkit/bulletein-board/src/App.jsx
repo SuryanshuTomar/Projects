17f83fe7328473af039a08c6bcea35333cfe778d
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-02-01 22:15:10 +0530

    converted the app from SPA to multi-page app

--- Redux-Toolkit/bulletein-board/src/App.jsx
+++ Redux-Toolkit/bulletein-board/src/App.jsx
@@ -1,12 +1,26 @@
 import AddPostForm from "./features/posts/AddPostForm";
 import PostsList from "./features/posts/PostsList";
+import SinglePostPage from "./features/posts/SinglePostPage";
+import Layout from "./components/Layout";
+
+import { Routes, Route } from "react-router-dom";
+import EditPostForm from "./features/posts/EditPostForm";
 
 function App() {
 	return (
-		<main className="App">
-			<AddPostForm />
-			<PostsList />
-		</main>
+		<Routes>
+			<Route path="/" element={<Layout />}>
+				<Route index element={<PostsList />} />
+
+				<Route path="post">
+					<Route index element={<AddPostForm />} />
+					<Route path=":postId" element={<SinglePostPage />} />
+					<Route path="edit">
+						<Route path=":postId" element={<EditPostForm />} />
+					</Route>
+				</Route>
+			</Route>
+		</Routes>
 	);
 }
 
