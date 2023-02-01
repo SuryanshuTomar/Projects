5be92e8d8cc89088c45baa26c81c92f355143b0c
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-02-01 22:15:45 +0530

    Added Browser Router and dispatch for fetchPost()

--- Redux-Toolkit/bulletein-board/src/main.jsx
+++ Redux-Toolkit/bulletein-board/src/main.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import "./index.css";
+import App from "./App";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
 import { fetchUsers } from "./features/users/usersSlice";
+import { fetchPosts } from "./features/posts/postsSlice";
+
+// fetching posts right when the app is starting
+store.dispatch(fetchPosts());
 
 // fetching users right when the app is starting
 store.dispatch(fetchUsers());
@@ -12,7 +18,14 @@ store.dispatch(fetchUsers());
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<App />
+			<BrowserRouter>
+				<Routes>
+					{/* 
+						This "/*" in the path will allow us to use nested routes inside our <App /> Component 
+					*/}
+					<Route path="/*" element={<App />} />
+				</Routes>
+			</BrowserRouter>
 		</Provider>
 	</React.StrictMode>
 );
