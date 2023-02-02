3ab4cd730dd6808ee7d8b2f7bf145d80ffd447c1
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-02-02 14:08:41 +0530

    added count to header

--- Redux-Toolkit/bulletein-board/src/components/Header.jsx
+++ Redux-Toolkit/bulletein-board/src/components/Header.jsx
@@ -1,6 +1,10 @@
+import { useDispatch, useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
+import { getCount, increaseCount } from "../features/posts/postsSlice";
 
 const Header = () => {
+	const dispatch = useDispatch();
+	const count = useSelector(getCount);
 	return (
 		<header className="Header">
 			<h1>Redux Blog</h1>
@@ -12,7 +16,12 @@ const Header = () => {
 					<li>
 						<NavLink to="post">Add a Post</NavLink>
 					</li>
+					<li>
+						<NavLink to="user">Users</NavLink>
+					</li>
 				</ul>
+
+				<button onClick={() => dispatch(increaseCount())}>Count: {count}</button>
 			</nav>
 		</header>
 	);
