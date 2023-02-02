d3c28ea55b45ffdf0dfa4a5046f28334374ef169
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-02-02 14:08:28 +0530

    added not found and users route

--- Redux-Toolkit/bulletein-board/src/App.jsx
+++ Redux-Toolkit/bulletein-board/src/App.jsx
@@ -3,8 +3,10 @@ import PostsList from "./features/posts/PostsList";
 import SinglePostPage from "./features/posts/SinglePostPage";
 import Layout from "./components/Layout";
 
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import EditPostForm from "./features/posts/EditPostForm";
+import UsersList from "./features/users/UsersList";
+import UserPage from "./features/users/UserPage";
 
 function App() {
 	return (
@@ -19,6 +21,16 @@ function App() {
 						<Route path=":postId" element={<EditPostForm />} />
 					</Route>
 				</Route>
+
+				<Route path="user">
+					<Route index element={<UsersList />} />
+					<Route path=":userId" element={<UserPage />} />
+				</Route>
+
+				{/* Catch all - replace with 404 component */}
+				{/* replace flag will replace the route which we tried to access but 
+				it doesn't exist in our application with "/" */}
+				<Route path="*" element={<Navigate to="/" replace />} />
 			</Route>
 		</Routes>
 	);
