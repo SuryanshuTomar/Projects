aa53095e7455dbe0fd8352e341ba053eea3a6d16
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-02-04 20:51:59 +0530

    added react  router to the app

--- React/admin-dashboard-lama/src/App.jsx
+++ React/admin-dashboard-lama/src/App.jsx
@@ -1,5 +1,26 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import List from "./pages/list/List";
+import Login from "./pages/login/Login";
+import New from "./pages/new/New";
+import Single from "./pages/single/Single";
+
 function App() {
-	return <div>App</div>;
+	return (
+		<div>
+			<Routes>
+				<Route path="/">
+					<Route index element={<Home />} />
+					<Route path="login" element={<Login />} />
+					<Route path="users">
+						<Route index element={<List />} />
+						<Route path="new" element={<New />} />
+						<Route path=":userId" element={<Single />} />
+					</Route>
+				</Route>
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
