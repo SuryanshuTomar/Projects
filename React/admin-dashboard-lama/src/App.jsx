a97122bd07b32d91f99cd8fddaa271acd0bffb8e
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-02-04 20:56:10 +0530

    added not found route

--- React/admin-dashboard-lama/src/App.jsx
+++ React/admin-dashboard-lama/src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/home/Home";
 import List from "./pages/list/List";
 import Login from "./pages/login/Login";
 import New from "./pages/new/New";
+import NotFound from "./pages/not-found/NotFound";
 import Single from "./pages/single/Single";
 
 function App() {
@@ -23,6 +24,7 @@ function App() {
 						<Route path="new" element={<New />} />
 					</Route>
 				</Route>
+				<Route path="*" element={<NotFound />} />
 			</Routes>
 		</div>
 	);
