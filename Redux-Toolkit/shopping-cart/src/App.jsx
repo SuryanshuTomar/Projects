f95e1fbe56c4c6c83560fcf5d067bfde5b397af1
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-02-05 13:53:57 +0530

    Added React Router

--- Redux-Toolkit/shopping-cart/src/App.jsx
+++ Redux-Toolkit/shopping-cart/src/App.jsx
@@ -1,4 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Cart from "./pages/Cart";
+import Home from "./pages/Home";
+
 const App = () => {
-	return <div>App</div>;
+	return (
+		<div>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/cart" element={<Cart />} />
+			</Routes>
+		</div>
+	);
 };
 export default App;
