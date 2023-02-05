ea3958eb6f04cf91eb320e775b5a1c6c0c9b65c4
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-02-05 15:57:15 +0530

    Cart Component

--- Redux-Toolkit/shopping-cart/src/pages/Cart.jsx
+++ Redux-Toolkit/shopping-cart/src/pages/Cart.jsx
@@ -1,4 +1,30 @@
+import { useDispatch, useSelector } from "react-redux";
+import { getCart, remove } from "../features/cartSlice";
+
 const Cart = () => {
-	return <div>Cart</div>;
+	const dispatch = useDispatch();
+	const cart = useSelector(getCart);
+
+	const handleRemove = (itemId) => {
+		dispatch(remove(itemId));
+	};
+
+	return (
+		<div>
+			<h3>Cart</h3>
+			<div className="cartWrapper">
+				{cart.cartItems.map((item) => (
+					<div className="cartCard" key={item.id}>
+						<img src={item.image} alt={item.title} />
+						<h5>{item.title}</h5>
+						<h5>{item.price}</h5>
+						<button className="btn" onClick={() => handleRemove(item.id)}>
+							Remove
+						</button>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 };
 export default Cart;
