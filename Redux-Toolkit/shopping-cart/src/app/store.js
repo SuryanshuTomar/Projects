a87615332fa223e93f24b178d291404d8d8a3ad7
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-02-07 18:18:24 +0530

    added products reducer

--- Redux-Toolkit/shopping-cart/src/app/store.js
+++ Redux-Toolkit/shopping-cart/src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "../features/cartSlice";
+import productsReducer from "../features/productSlice";
 
 const store = configureStore({
 	reducer: {
 		cart: cartReducer,
+		products: productsReducer,
 	},
 });
 
