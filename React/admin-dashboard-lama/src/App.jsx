faa7c26b93ec392562cbbdd1e9d0f1becaeee17e
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-02-19 19:41:29 +0530

    added the dark Mode style

--- React/admin-dashboard-lama/src/App.jsx
+++ React/admin-dashboard-lama/src/App.jsx
@@ -1,15 +1,20 @@
-import { Routes, Route } from "react-router-dom";
-import { userInputs } from "./data/formSource";
+import { useContext } from "react";
+import { Route, Routes } from "react-router-dom";
+import { DarkModeContext } from "./context/darkModeContext";
+import { productInputs, userInputs } from "./data/formSource";
 import Home from "./pages/home/Home";
 import List from "./pages/list/List";
 import Login from "./pages/login/Login";
 import New from "./pages/new/New";
 import NotFound from "./pages/not-found/NotFound";
 import Single from "./pages/single/Single";
+import "./styles/darkmode.scss";
 
 function App() {
+	const { darkMode } = useContext(DarkModeContext);
+
 	return (
-		<div>
+		<div className={darkMode ? "app dark" : "app"}>
 			<Routes>
 				<Route path="/">
 					<Route index element={<Home />} />
@@ -17,14 +22,19 @@ function App() {
 					<Route path="users">
 						<Route index element={<List />} />
 						<Route path=":userId" element={<Single />} />
-						<Route path="new" element={<New />} />
+						<Route
+							path="new"
+							element={<New title="Add new user" inputs={userInputs} />}
+						/>
 					</Route>
 					<Route path="products">
 						<Route index element={<List />} />
 						<Route path=":productId" element={<Single />} />
 						<Route
 							path="new"
-							element={<New title="Add new user" inputs={userInputs} />}
+							element={
+								<New title="Add new product" inputs={productInputs} />
+							}
 						/>
 					</Route>
 				</Route>
