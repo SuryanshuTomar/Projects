9b50b9287ada201ccfab98151c472be8e80cb741
SuryanshuTomar <suryanshutomar123@outlook.com>
2023-02-19 19:40:56 +0530

    add logic for dark and light mode onClick

--- React/admin-dashboard-lama/src/components/sidebar/Sidebar.jsx
+++ React/admin-dashboard-lama/src/components/sidebar/Sidebar.jsx
@@ -1,22 +1,30 @@
-import "./sidebar.scss";
 import DashboardIcon from "@mui/icons-material/Dashboard";
-import Person2Icon from "@mui/icons-material/Person2";
-import Inventory2Icon from "@mui/icons-material/Inventory2";
-import PointOfSaleOutlinedIcon from "@mui/icons-material/PointOfSaleOutlined";
-import LocalShippingIcon from "@mui/icons-material/LocalShipping";
-import QueryStatsIcon from "@mui/icons-material/QueryStats";
-import NotificationsIcon from "@mui/icons-material/Notifications";
-import MedicalInformationIcon from "@mui/icons-material/MedicalInformation";
-import PsychologyIcon from "@mui/icons-material/Psychology";
-import SettingsIcon from "@mui/icons-material/Settings";
 import FaceIcon from "@mui/icons-material/Face";
+import Inventory2Icon from "@mui/icons-material/Inventory2";
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
 import LogoutIcon from "@mui/icons-material/Logout";
+import MedicalInformationIcon from "@mui/icons-material/MedicalInformation";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import Person2Icon from "@mui/icons-material/Person2";
+import PointOfSaleOutlinedIcon from "@mui/icons-material/PointOfSaleOutlined";
+import PsychologyIcon from "@mui/icons-material/Psychology";
+import QueryStatsIcon from "@mui/icons-material/QueryStats";
+import SettingsIcon from "@mui/icons-material/Settings";
+import "./sidebar.scss";
+
+import { useContext } from "react";
+import { Link, NavLink } from "react-router-dom";
+import { DarkModeContext } from "../../context/darkModeContext";
 
 const Sidebar = () => {
+	const { dispatch } = useContext(DarkModeContext);
+
 	return (
 		<div className="sidebar">
 			<div className="top">
-				<span className="logo">WebMatrix - Admin</span>
+				<NavLink to="/" style={{ textDecoration: "none" }}>
+					<span className="logo">Matrix - Admin</span>
+				</NavLink>
 			</div>
 
 			<hr />
@@ -29,14 +37,24 @@ const Sidebar = () => {
 					</li>
 
 					<p className="title">LISTS</p>
-					<li>
-						<Person2Icon className="icon" />
-						<span>Users</span>
-					</li>
-					<li>
-						<Inventory2Icon className="icon" />
-						<span>Products</span>
-					</li>
+					<Link
+						to="/users"
+						style={{ textDecoration: "none", color: "gray" }}
+					>
+						<li>
+							<Person2Icon className="icon" />
+							<span>Users</span>
+						</li>
+					</Link>
+					<Link
+						to="/products"
+						style={{ textDecoration: "none", color: "gray" }}
+					>
+						<li>
+							<Inventory2Icon className="icon" />
+							<span>Products</span>
+						</li>
+					</Link>
 					<li>
 						<PointOfSaleOutlinedIcon className="icon" />
 						<span>Orders</span>
@@ -82,8 +100,14 @@ const Sidebar = () => {
 				</ul>
 			</div>
 			<div className="bottom">
-				<div className="colorOption"></div>
-				<div className="colorOption"></div>
+				<div
+					className="colorOption"
+					onClick={() => dispatch({ type: "LIGHT" })}
+				></div>
+				<div
+					className="colorOption"
+					onClick={() => dispatch({ type: "DARK" })}
+				></div>
 			</div>
 		</div>
 	);
